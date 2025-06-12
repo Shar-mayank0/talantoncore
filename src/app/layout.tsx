332f72fcda3023614a7903bfb91969f1c4e7cba7
Mayank Sharma <shar.mayankofficial@gmail.com>
2025-06-12 16:59:23 +0530

    fix: Update deployment script to remove fallback for pm2 restart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,26 +10,26 @@ export const viewport: Viewport = {
     themeColor: "#fcb716",
 }
 
-export const metadata: Metadata = {
-  title: "Internship Assignment",
-  description: "assignment for TalantonCore  |  Software Development internship",
-  keywords: ["internship", "assignment", "software development", "TalantonCore"],
-  authors: [{ name: "Your Name" }],
-  creator: "Your Name",
-  publisher: "TalantonCore",
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
+// export const metadata: Metadata = {
+//   title: "Internship Assignment",
+//   description: "assignment for TalantonCore  |  Software Development internship",
+//   keywords: ["internship", "assignment", "software development", "TalantonCore"],
+//   authors: [{ name: "Your Name" }],
+//   creator: "Your Name",
+//   publisher: "TalantonCore",
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
 
-};
+// };
 
 
 export default function RootLayout({
